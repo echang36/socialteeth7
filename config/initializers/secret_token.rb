df940e9d16c32570972e74c0fa7edf6f47b80e50
Elaine Chang <elainefchang@gmail.com>
2012-04-22 18:52:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialteeth7::Application.config.secret_token = 'f503e08ffbd7bdc5edbe8ab87bd299f162562d35f0e5c75a6c36e9a43d84a7782ac83406cbb8e5e4977f2d2b36932122848ba8130992523a1eb2e782565f8102'
